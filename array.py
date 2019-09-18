60bcf2289893d8d866613dfab7a509075cab6c7a
yogithesymbian <yogirenbox33@gmail.com>
2019-09-19 02:24:17 +0800

    give-function-redudant

--- array.py
+++ array.py
@@ -17,8 +17,6 @@ rows, cols = (5, 6)
 # initialize the arr for arr[position][position]
 arr = [[0 for i in range(cols)] for j in range(rows)]
 
-isi = 0  # init for arr value (increment)
-
 
 def flowAssignTest():  # log flow assign testing
     for i in range(rows):
@@ -27,19 +25,69 @@ def flowAssignTest():  # log flow assign testing
             # print(i, j)  # horizontal left to right
             # print(x, x) # horitzontal left to right then right to left
 
-
             # print(j, i)  # vertical down from left to right
             # print(x, x) # vertical down from left to right then up
             # vertical <--------
             # but u will get the error index out of bound, so u need change the rows to cols and else too
 
-            
             # obat nyamuk
             if i == 0:
                 print(i, j)
-            if i == 1:
-                if j == rows:
+            if j == rows:
+                # if i == 1:
+                #     print(i, j)
+                # if i == 2:
+                #     print(i, j)
+                # if i == 3:
+                #     print(i, j)
+                if i != 0 and i != rows - 1:
                     print(i, j)
+
+    for i in range(rows):
+        for j in reversed(range(cols)):
+            # j = [j]
+            # j.reverse()
+            # j.__reversed__
+            if i == rows - 1:
+                print(i, j)
+                # downToLeft = [j]
+                # downToLeft.sort(reverse=True)
+                # print(downToLeft)
+                # print(sorted(list([j]), reverse=True))
+    for i in reversed(range(rows)):
+        for j in range(cols):
+            if j == 0:
+                if i != rows-1 and i != 0:
+                    print(i, 0)
+
+    for i in range(rows):
+        for j in range(cols):
+            if (i != rows - 1 and i != 0 and i != 1) and (j != 0 and j != cols - 1):
+                if i != rows - 2:
+                    print(i, j)
+                # if i != rows - 3:
+                #     print(i, j)
+
+    for i in range(rows):
+        for j in reversed(range(cols)):
+            if (i != rows - 1 and i != 0 and i != 1) and (j != 0 and j != cols - 1):
+                if i != rows - 3:
+                    print(i, j)
+                    # end of flow obat nyamuk
+
+
+def resultArray(data1, data2):
+    isi = 0  # init for arr value (increment)
+    for i in range(data1):
+        for j in range(data2):
+            isi += 1
+            arr[j][i] = isi
+
+    # arr[0][5] = 1
+    # show the array
+    for row in arr:
+        print("""
+            """, row)
 
 
 def arrMethod1(isi):
@@ -48,18 +96,8 @@ def arrMethod1(isi):
     =================METHOD 1 HORIZONTAL=======================
 
     """)
-
     # method 1
-    for i in range(rows):  # horizontal
-        for j in range(cols):
-            isi += 1
-            arr[i][j] = isi
-
-    # arr[0][5] = 1
-    # show the array
-    for row in arr:
-        print("""
-            """, row)
+    resultArray(rows, cols)  # horizontal resultArray(i, j)
 
 
 def arrMethod2(isi):
@@ -69,10 +107,35 @@ def arrMethod2(isi):
 
         """)
     # method 2
-    for i in range(cols):  # vertical
-        for j in range(rows):
+    ##
+    # just invert the first method
+    # to get vertical flow assign
+    # ##
+    resultArray(cols, rows)  # vertical resultArray(j,i)
+
+
+def arrMethod3(isi):
+    print("""
+
+        =================METHOD 3 CAPSULE=======================
+
+        """)
+    # method 3
+    for i in range(rows):  # vertical
+        for j in range(cols):
             isi += 1
-            arr[j][i] = isi
+            # arr[i][j] = isi
+            # obat nyamuk
+            if i == 0:  # top assign
+                arr[i][j] = isi
+            if j == rows:  # End Assign rows.length
+                # if i == 1:
+                #     print(i, j)
+                # if i == 2:
+                #     print(i,j)
+                arr[i][j] = isi
+            if i == cols:
+                print(i, j)
 
     # show the array
     for row in arr:
@@ -81,10 +144,10 @@ def arrMethod2(isi):
 
 
 def start():
-    print("hello world")
-    flowAssignTest()
+    # flowAssignTest()
     # arrMethod1(0)
-    # arrMethod2(0)
+    arrMethod2(0)
+    # arrMethod3(0)
 
 
 if __name__ == '__main__':
